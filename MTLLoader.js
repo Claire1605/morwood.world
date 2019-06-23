269f6aab54c2ee690197e355aca5df8976f61c13
Claire1605 <claire1605@gmail.com>
2019-06-23 11:29:03 +0100

    Fix typo

--- MTLLoader.js
+++ MTLLoader.js
@@ -513,6 +513,7 @@ THREE.MTLLoader.MaterialCreator.prototype = {
 		}
 
 		this.materials[ materialName ] = new THREE.MeshBasicMaterial( params );
+		this.materials[ materialName ].color = new THREE.Color( 0xffffff );
 		return this.materials[ materialName ];
 
 	},
